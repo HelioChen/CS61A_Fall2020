043a10973e859f833227a2594ede161883d819e6
HelioChen <1874449245@qq.com>
2024-07-29 21:15:17 +0800

    2024/7/29 second commit

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -316,14 +316,21 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
     """
     # BEGIN PROBLEM 10
     # Remove this line once implemented.
-    
+    if boar_brawl(score, opponent_score) >= threshold:
+        return 0
+    else:
+        return num_rolls
     # END PROBLEM 10
 
 
 def sus_strategy(score, opponent_score, threshold=11, num_rolls=6):
     """This strategy returns 0 dice when your score would increase by at least threshold."""
     # BEGIN PROBLEM 11
-    return num_rolls  # Remove this line once implemented.
+    # Remove this line once implemented.
+    if sus_update(0, score, opponent_score) - score >= threshold:
+        return 0
+    else:
+        return num_rolls
     # END PROBLEM 11
 
 
@@ -333,7 +340,7 @@ def final_strategy(score, opponent_score):
     *** YOUR DESCRIPTION HERE ***
     """
     # BEGIN PROBLEM 12
-    return 6  # Remove this line once implemented.
+    # Remove this line once implemented.
     # END PROBLEM 12
 
 
